1264c522f22c8725b6e185ac6d0b4e99ac94af07
Choi Hyunjin <44575072+hjchoi0207@users.noreply.github.com>
2023-07-24 23:24:50 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,48 +1,46 @@
 #include <iostream>
 #include <stack>
-#include <string>
 
 using namespace std;
 
+/* stack문제
+ * https://www.acmicpc.net/problemset?sort=ac_desc&algo=71
+*/
+
 bool isBalanced(string s) {
-    stack<char> myStack;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '(' || s[i] == '[') {
-            myStack.push(s[i]);
-        } else if (s[i] == ')' || s[i] == ']') {
-            if (myStack.empty()) {
-                return false;
-            }
+  stack<char> myStack;
+  
+  for (int i = 0; i < s.length(); i++) {
+      if (s[i] == '(' || s[i] == '[') {
+          myStack.push(s[i]);
+      } else if (s[i] == ')' || s[i] == ']') {
+          if (myStack.empty()){
+            return false;
+          }
+        
+          if (s[i] == ')') {
+              if (myStack.top() != '(') return false;
+              else myStack.pop();
+          } else if (s[i] == ']') {
+              if (myStack.top() != '[') return false;
+              else myStack.pop();
+          }
+      }
+  }
 
-            if (s[i] == ')') {
-                if (myStack.top() != '(') {
-                    return false;
-                } else {
-                    myStack.pop();
-                }
-            } else if (s[i] == ']') {
-                if (myStack.top() != '[') {
-                    return false;
-                } else {
-                    myStack.pop();
-                }
-            }
-        }
-    }
-
-    return myStack.empty();
+  return myStack.empty();
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-    string s;
-
-    while (true) {
+  string s;
+  
+ while (true) {
         getline(cin, s);
-        if (s == ".") 
+        if (s == ".") // 입력이 "."인 경우 루프 종료
             break;
 
         if (isBalanced(s)) {
@@ -51,6 +49,5 @@ int main() {
             cout << "no" << '\n';
         }
     }
-
-    return 0;
+  
 }
